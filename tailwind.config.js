cf2e19a7897ff3a048bad2645666a6c6d0008b42
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-02-19 16:03:10 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': 'Nunito Sans'
+    },
+    colors: {
+      'primary': '#232931',
+      'accent': '#08FFC8',
+      'white': '#FFF7F7',
+      'secondary': '#4A5463',
+    },
+  },
+  plugins: [],
+}
